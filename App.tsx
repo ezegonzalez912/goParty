6bf84af3f3b36222b2915075ededcefd78d66984
Juan Manuel Sanchez Diaz <jmsanchezdiaz02@gmail.com>
2022-01-04 01:03:52 -0300

    added router

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Main from './src/screens/Main';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Main />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <View style={styles.container}>
+          <Stack.Navigator>
+            <Stack.Screen name='Main' component={Main} />
+          </Stack.Navigator>
+        </View>
+    </NavigationContainer>
+    
   );
 }
 
